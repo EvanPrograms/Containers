696cf482ff00662d7d0c9439090f1b43afb9009d
Evan Perry <eperry2688@gmail.com>
2024-10-10 16:06:17 -0600

    12.21 Full dev functionality, starting 12.22

--- bloglistContainers/bloglist-frontend/vite.config.js
+++ bloglistContainers/bloglist-frontend/vite.config.js
@@ -5,12 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://server:3003',
         changeOrigin: true,
       },
     },
+  },
+  build: {
+    outDir: 'dist',
   },
   test: {
     environment: 'jsdom',
